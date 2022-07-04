0f759a0a3b1723345975e5975417d61d909f9ecb
xEgoist <egoist@egoistic.dev>
2022-07-03 19:05:27 -0500

    implemented all functionalities of the emulator. Next => SDL2 and the biiiiig switch statement (gonna use if statement because fuck me that's why)

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,31 @@
 const std = @import("std");
-
+const expectEqual = std.testing.expectEqual;
 
 const MEM_SIZE: u16 = 0x1000;
 
-const SCREEN_WIDTH: u8 = 64;
-const SCREEN_HEIGHT: u8 = 32;
+const SCREEN_WIDTH: u16 = 64;
+const SCREEN_HEIGHT: u16 = 32;
+const FONTSET_START_ADDRESS: u16 = 0x50;
+
+const font = [_]u8{
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+};
+
 
 
 const C8Error = error {
@@ -12,12 +33,13 @@ StackPointer,
 OutOfBound,
 UnImplemented,
 };
-
 const Components = struct {
 c: u8,
 x: u8,
 y: u8,
 d: u8,
+nn: u16,
+nnn: u16,
 };
 
 const Chip8 = struct {
@@ -29,31 +51,65 @@ stack: [16]u16 = [_]u16{0} ** 16,
 sp: u8 = 0,
 dt: u8 = 0,
 st: u8 = 0,
-keypad: u16 = 0,
-fb: [256]u8 = [_]u8 {0} ** 256,
+keypad: [16]u8 = [_]u8{0} ** 16,
+fb: [SCREEN_HEIGHT*SCREEN_WIDTH]u16 = [_]u16 {0} ** (SCREEN_WIDTH * SCREEN_HEIGHT),
 tone: bool = false,
 time: isize = 0,
-
+  
+  // Utility Function 
   fn read_opcode(self: *Chip8) u16 {
     var op_byte1 = @intCast(u16,self.mem[self.pc]);
     var op_byte2 = @intCast(u16,self.mem[self.pc + 1]);
     return op_byte1 << 8 | op_byte2;
 
   }
-  pub fn ret(self: *Chip8) C8Error!void {
+  // Clear Screen
+  fn cls(self: *Chip8) void {
+    comptime { 
+    self.fb = [_]u16 {0} ** (SCREEN_WIDTH * SCREEN_HEIGHT);
+    }
+  }
+  // return from subroutine
+  fn ret(self: *Chip8) C8Error!void {
     if (self.sp == 0) {
      return C8Error.StackPointer;
     }
     self.sp -= 1;
     self.pc = self.stack[self.sp];
   }
+  
 
-  fn add(self: *Chip8, x: u8, y: u8) C8Error!void {
-    var arg1 = self.v[x];
-    var arg2 = self.v[y];
-    self.v[x] = arg1 + arg2;
+  // 1nnn jump to addr nnn
+  // Takes NNN as argument. 
+  fn jp(self: *Chip8, addr: u16) void {
+    self.pc = addr;
   }
 
+  
+  // 8xy4
+  // Add two registries together, stores result in vx
+  fn addxy(self: *Chip8, x: u8, y: u8) void {
+    var sum: u16 = self.v[x];
+    sum += self.v[y];
+    if (sum > 255) {
+      self.v[0xF] = 1;
+    }
+    else {
+      self.v[0xF] = 0;
+    }
+    self.v[x] = @intCast(u8,sum & 0xFF);
+  }
+  
+  // skip if no keypress
+  fn sknp(self: *Chip8, x: u8) void {
+    var key = self.v[x];
+    if (self.keypad[key] != 1) {
+      self.pc +=2;
+    }
+
+  }
+  
+  // call function by incrementing the stack pointer and pc to the top of the stack.
   fn call(self: *Chip8, addr: u16) C8Error!void {
     if (self.sp > self.stack.len) {
       return C8Error.OutOfBound;
@@ -62,6 +118,221 @@ time: isize = 0,
     self.sp +=1;
     self.pc = addr;
   }
+  //3xkk
+  fn sex(self: *Chip8, x: u8,  kk: u8) void {
+    if (self.v[x] == self.v[kk]) {
+    self.pc+=2;   
+    }
+  }
+
+  //4xkk
+  fn snex(self: *Chip8, x: u8, kk: u8) void {
+    if (self.v[x] != self.v[kk]) {
+      self.pc+=2;
+    }
+  }
+  //5xy0
+  fn sexy(self: *Chip8, x: u8, y: u8) void {
+    if (self.v[x] == self.v[y]) {
+      self.pc +=2;
+    }
+  }
+  //6xkk
+  fn ldx(self: *Chip8, x: u8, kk: u8) void {
+    self.v[x] = kk;
+  }
+  //7xkk
+  // note, this has no carry.
+  fn addx(self: *Chip8, x: u8, kk: u8) void {
+   self.v[x] += kk;
+  }
+
+  // 8xy0
+  fn ldxy(self: *Chip8, x: u8, y: u8) void {
+  self.v[x] = self.v[y];
+  }
+  
+  // 8xy1
+  fn orxy(self: *Chip8, x: u8, y: u8) void {
+    self.v[x] |= self.v[y];
+  }
+
+  // 8xy2
+  fn andxy(self: *Chip8, x: u8, y: u8) void {
+    self.v[x] &= self.v[y];
+  }
+  
+  // 8xy5
+  fn subxy(self: *Chip8, x: u8, y: u8) void {
+    if (self.v[x] > self.v[y]) {
+      self.v[0xF] = 1;
+    }
+    else {
+      self.v[0xF] = 0;
+    }
+    self.v[x] -%= self.v[y];
+  }
+
+  //8xy6
+  fn shrx(self: *Chip8, x: u8) void {
+    if (self.v[x] & 1 == 1) {
+      self.v[0xF] = 1;
+    }
+    else {
+      self.v[0xF] = 0;
+    }
+    self.v[x] >>= 1;
+  }
+  //8xy7
+  fn subnxy(self: *Chip8, x: u8, y:u8) void {
+    if (self.v[y] > self.v[x]) {
+      self.v[0xF] = 1;
+    }
+    else {
+      self.v[0xF] = 0;
+    }
+    self.v[x] = self.v[y] -% self.v[x];
+  }
+  //8xyE
+  fn shlx(self: *Chip8, x: u8) void {
+    if (self.v[x] & 1 == 1 ) {
+      self.v[0xF] = 1;
+    }
+    else {
+      self.v[0xF] = 0;
+    }
+    self.v[x] <<= 1;
+  }
+
+  //9xy0
+  fn snexy(self: *Chip8, x: u8, y: u8) void {
+    if (self.v[x] != self.v[y]) {
+      self.pc  +=2;
+    }
+  }
+  //Annn
+  fn ldi(self: *Chip8, nnn: u16) void {
+    self.i = nnn;
+  }
+
+  //Bnnn
+  fn jpv0(self: *Chip8, nnn: u16) void {
+    self.pc = nnn + self.v[0];
+  }
+
+  //Cxkk
+  fn rndvx(self: *Chip8, x: u8, kk: u8 ) void {
+    var seed = std.time.timestamp();
+    if (seed < 0) {
+      seed = 42;
+    }
+    var rand = std.rand.DefaultPrng.init(@intCast(u64,seed));
+    const num = rand.random().int(u8);
+    self.v[x] = num & kk;
+  }
+  //dxyn
+  fn drwxyn(self: *Chip8, x: u8, y: u8, nibble: u8) void {
+    var xpos = self.v[x] % SCREEN_WIDTH;
+    var ypos = self.v[y] % SCREEN_HEIGHT;
+    self.v[0xF] = 0;
+    var row = 0;
+
+    while (row < nibble) : (row +=1) {
+      var col = 0;
+      var sprite_byte = self.mem[self.i + row];
+      while (col < 8) : (col +=1) {
+        var sprite_pixel = sprite_byte & (0x80 >> col);
+        var screen_pixel = *self.fb[(ypos + row) * SCREEN_WIDTH + (xpos + col)];
+        if (sprite_pixel == 1) {
+          if (screen_pixel.* == 0xFFFFFFFF) {
+            self.v[0xF] = 1;
+          }
+          screen_pixel.* ^= 0xFFFFFFFF;
+        }
+      }
+    }
+  }
+  //ex9e
+  fn skpx(self: *Chip8, x: u8) void {
+    if (self.keypad[x] == 1){
+      self.pc +=2;
+    }
+  }
+
+  //exA1
+  fn sknpx(self: *Chip8, x: u8) void {
+    if (self.keypad[x] == 0) {
+      self.pc +=2;     
+    }
+  }
+
+  //fx07
+  fn ldxdt(self: *Chip8, x: u8) void {
+    self.v[x] = self.dt;
+  }
+
+
+  //fx0A
+  fn ldxk(self: *Chip8, x: u8) void {
+    var k = 0;
+    while (k <= 15) : (k += 1) {
+      if (self.keypad[k] == 1) {
+        self.v[x] = k;
+        return;
+      }
+    }
+    self.pc -= 2;
+  }
+
+  //fx15
+  fn lddtx(self: *Chip8, x: u8) void {
+    self.dt = self.v[x];
+  }
+
+  //fx18
+  fn ldstx(self: *Chip8, x: u8) void {
+    self.st = self.v[x];
+  }
+
+  //fx1e
+  fn addix(self: *Chip8, x: u8) void {
+    self.i += self.v[x];
+  }
+
+  //fx29
+  fn ldfx(self: *Chip8, x: u8) void {
+    self.i = FONTSET_START_ADDRESS + (5*self.v[x]);
+  }
+  //fx33
+  fn ldbx(self: *Chip8, x: u8) void {
+    var val = self.v[x];
+    self.mem[self.i + 2] = self.v[x] % 10;
+    val /= 10;
+
+    self.mem[self.i + 1] = self.v[x] % 10;
+    val /= 10;
+
+    self.mem[self.i] = self.v[x] % 10;
+  }
+  
+  //fx55
+  fn ldix(self: *Chip8, x: u8) void {
+    for (self.mem[self.i..x+1]) |*val,idx| {
+      val.* = self.v[idx];
+    }
+  }
+
+  //fx65
+  fn ldxi(self: *Chip8, x: u8) void {
+    for (self.v[0..x+1]) |*val, idx| {
+      val.* = self.mem[idx + self.i];
+    }
+  }
+  
+
+
+
+
   fn run(self: *Chip8) C8Error!void {
     while (true){
     var opcode = self.read_opcode();
@@ -71,8 +342,9 @@ time: isize = 0,
     .x = @intCast(u8,(opcode & 0x0F00) >> 8),
     .y = @intCast(u8,(opcode & 0x00F0) >> 4),
     .d = @intCast(u8,(opcode & 0x000F) >> 0),
+    .nn = opcode & 0xFF,
+    .nnn = opcode & 0xFFF,
     };
-    var nnn = opcode & 0xFFF;
     if (comps.c == 0 and comps.x == 0 and comps.y == 0 and comps.d == 0) {
       //std.debug.print("DONE..\n", .{});
         return;
@@ -86,12 +358,18 @@ time: isize = 0,
     }
     if (comps.c == 0x2 ) {
       //std.debug.print("NNNN..\n", .{});
-        try self.call(nnn);
+        try self.call(comps.nnn);
     }
     if (comps.c == 0x8 and comps.d == 0x4) {
       //std.debug.print("ADDING..\n", .{});
-      try self.add(comps.x,comps.y);
+      self.addxy(comps.x,comps.y);
       continue;
+    }
+    if (self.dt > 0) {
+      self.dt -=1;
+    }
+    if (self.st > 0) {
+      self.st -=1;
     }
   }
   }
@@ -126,10 +404,61 @@ pub fn main() anyerror!void {
  try comptime cpu.run();
  std.debug.print("{}",.{cpu.v[0]});
 
+}
 
 
+//TODO 2: Test All Funcs 
+
+
+test "sub x y test (8xy5)" {
+  var cpu = Chip8 {
+  };
+  cpu.v[0] = 0x2D;
+  cpu.v[1] = 0x4B;
+  cpu.subxy(0,1);
+  try expectEqual(@as(u8,0xE2), cpu.v[0]);
+}
+
+test "8xy4 test (addxy)" {
+  var cpu = Chip8 {
+  };
+
+  // Carry
+  cpu.v[0] = 0xED;
+  cpu.v[1] = 0x4B;
+  cpu.addxy(0,1);
+  try expectEqual(@as(u8,0x38), cpu.v[0]);
+  try expectEqual(@as(u8,1), cpu.v[0xF]);
+  // No Carry 
+  cpu.v[0] = 0x2D;
+  cpu.v[1] = 0x4B;
+  cpu.addxy(0,1);
+  try expectEqual(@as(u8,0x78), cpu.v[0]);
+  try expectEqual(@as(u8,0), cpu.v[0xF]);
+
+}
+
+
+test "shlx test" {
+ var cpu = Chip8 {};
+ cpu.v[0] = 5;
+ cpu.shlx(0);
+ try expectEqual(@as(u8,10), cpu.v[0]);
+ try expectEqual(@as(u8,1), cpu.v[0xF]);
 }
 
 test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    try expectEqual(10, 3 + 7);
 }
+
+test "fx55 correctness test" {
+    var cpu = Chip8 {};
+    cpu.v[0] = 5;
+    cpu.v[1] = 6;
+    cpu.v[2] = 7;
+    cpu.v[3] = 8;
+    // Deliberately not including the 8 to test the idx.
+    cpu.ldix(2);
+    try expectEqual(cpu.mem[0..5].*, [_]u8{5,6,7,0,0});
+}
+
